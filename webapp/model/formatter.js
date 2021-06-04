f78d72d78361aa6ebb4f7b55d6deb278d200489d
Maarten Exelmans <maarten.exelmans@dimensys.pro>
2021-06-04 16:49:06 +0200

    Added decimal formatting for measurement values

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -9,12 +9,38 @@ sap.ui.define([], function () {
 		 * @param {string} sValue the number string to be rounded
 		 * @returns {string} sValue with 2 digits rounded
 		 */
-		numberUnit : function (sValue) {
+		numberUnit: function (sValue) {
 			if (!sValue) {
 				return "";
 			}
 			return parseFloat(sValue).toFixed(2);
-		}
+		},
+
+		decimalFormatter: sap.ui.model.CompositeType.extend("number", {
+			constructor: function () {
+				sap.ui.model.CompositeType.apply(this, arguments);
+				this.bParseWithValues = true; // make 'parts' available in parseValue
+				//this.bUseRawValues = true;
+			},
+
+			/**
+			 * Displaying data from the right model (model -> view)
+			 */
+			formatValue: function (parts) {
+				return parts && parts[0] ? parseFloat(parts[0]).toFixed(parts[1]) : null;
+			},
+
+			/**
+			 * Assigning entered value to the right model (view -> model)
+			 */
+			parseValue: function (enteredValue, stuff, parts) {
+				return (parts && parts[1] && (!isNaN(parts[1]) || parts[1] === 0)) ? [parseFloat(enteredValue).toFixed(parts[1]), parts[1]] : [
+					parseFloat(enteredValue).toFixed(3), 3
+				];
+			},
+
+			validateValue: () => true // Nothing to validate here
+		})
 
 	};
 
